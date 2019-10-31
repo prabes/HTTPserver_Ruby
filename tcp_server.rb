2d696309fd72fa318374d5024464cfa3ad1a5c1f
rdeyvil <rdeyvil11@gmail.com>
2019-10-31 16:10:43 +0545

    Minimal Ruby HTTP-server

--- tcp_server.rb
+++ tcp_server.rb
@@ -3,6 +3,13 @@ require 'socket'
 server = TCPServer.new 5678
 
 while session = server.accept
-  session.puts "Hello World! The time is #{Time.now}"
+  request = session.gets 
+  puts request
+
+  session.puts "HTTP/1.1 200\r\n"
+  session.puts "Content-Type: text/html\r\n"
+  session.puts "\r\n"
+  session.puts "Hello World! The time is #{Time.now}" 
+  
   session.close 
-end
+end 
